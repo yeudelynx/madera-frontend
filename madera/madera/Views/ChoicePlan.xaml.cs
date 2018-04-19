09dc42442455d2c9b66fa5d207bd461808b7907a
crawood <monsbrot.anthonin@gmail.com>
2018-04-20 00:07:04 +0200

    create user + choice plan

--- madera/madera/Views/ChoicePlan.xaml.cs
+++ madera/madera/Views/ChoicePlan.xaml.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Android.Widget;
+using madera.Helpers;
+using madera.Models;
+using Newtonsoft.Json;
+using SQLite;
+using System.IO;
+
 namespace madera.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,7 +22,20 @@ namespace madera.Views
 		public ChoicePlan ()
 		{
 			InitializeComponent ();
-		}
+
+            LocalDatabase db = new LocalDatabase();
+            db.tableSol.Select(x => x).ToList();
+
+            foreach (var sol in db.tableSol)
+            {
+                //Console.WriteLine(" ++++++ USER LOGIN" + user.id + " : " + user.login);
+                Xamarin.Forms.Button MyControl1 = new Xamarin.Forms.Button();
+                MyControl1.Text = "Sol" + sol.id;
+                MyControl1.Clicked += choice_plan;
+
+                btn_sols.Children.Add(MyControl1);
+            }
+        }
 
         public void choice_plan(object sender, EventArgs e)
         {
--- madera/madera/Views/CreateUser.xaml.cs
+++ madera/madera/Views/CreateUser.xaml.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Android.Widget;
+using madera.Helpers;
+using madera.Models;
+using Newtonsoft.Json;
+using SQLite;
+using System.IO;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -19,9 +26,26 @@ namespace madera.Views
 
         public void validate_user(object sender, EventArgs e)
         {
-            var page_choice_plan = new ChoicePlan();
-            Navigation.PushAsync(page_choice_plan);
-            NavigationPage.SetHasNavigationBar(page_choice_plan, false);
+            if (entry_nom.Text != "" && entry_prenom.Text != "" && entry_mail.Text != "" && entry_telephone.Text != "" && entry_adresse.Text != "") {
+
+                string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "madera.db3");
+                var db = new SQLiteConnection(dbPath);
+
+                Client client = new Client {
+                    nom = entry_nom.Text,
+                    prenom = entry_prenom.Text,
+                    mail = entry_mail.Text,
+                    tel = entry_telephone.Text,
+                    adresse = entry_adresse.Text
+                };
+
+                db.Insert(client);
+
+                var page_choice_plan = new ChoicePlan();
+                Navigation.PushAsync(page_choice_plan);
+                NavigationPage.SetHasNavigationBar(page_choice_plan, false);
+
+            }
 
         }
     }
