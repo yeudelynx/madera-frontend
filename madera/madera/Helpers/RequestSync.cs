7d6339bb53d1107a1ece9bbae7e61a17fb021779
Matthieu Pauly <yeudelynx@hotmail.com>
2018-04-21 11:37:58 +0200

    test sync

--- madera/madera/Helpers/RequestSync.cs
+++ madera/madera/Helpers/RequestSync.cs
@@ -22,9 +22,10 @@ namespace madera.Helpers
             String datetimestr = DateTime.ParseExact(date.date, "yyyy-MM-dd HH:mm:ss.ffffff", CultureInfo.InvariantCulture).ToString("yyyy-MM-dd HH:mm:ss");
             //strftime('%s', date);
 
-            db.db.Insert(new Client { id = 1000, adresse = "addr", nom = "nom", prenom = "prenom", mail = "mail@mail.mail", tel = "06.06.06.06.06", updated_at = "2018-04-20 12:36:00" });
-            db.db.Insert(new Client { id = 1000, adresse = "addr", nom = "nom", prenom = "prenom", mail = "mail@mail.mail", tel = "06.06.06.06.06", updated_at = "2018-04-20 12:36:00" });
-            db.db.Insert(new Client { id = 1000, adresse = "addr2", nom = "nom2", prenom = "prenom2", mail = "mail@mail.mail2", tel = "06.06.06.06.062", updated_at = "2018-04-20 17:36:00" });
+            //test insert in server DB
+            db.db.Insert(new Client { id = 1000, adresse = "addr", nom = "nom", prenom = "prenom", mail = "mail@mail.mail", tel = "06.06.06.06.06", updated_at = "2018-05-20 12:36:00" });
+            db.db.Insert(new Constituer { id = 1000, x_pos = 100, y_pos = 1000, z_pos = 999, etage_plan = 5, prix_module = 123456789, module_id = 1, devis_id = 1, couleur_id = 1, matiere_id = 1, unite_id = 1, updated_at = "2018-05-20 12:36:00" });
+            db.db.Insert(new Devis { id = 1000, is_sync = true, orientation = 123, client_id = 1, user_id = 1, sol_id = 1, updated_at = "2018-05-20 17:36:00" });
 
             //Get all news or updated clients, devis & constituers, after LastSyncDate
             List<Client> clients = db.tableClient.ToList().Where(c => DateTime.Parse(c.updated_at) > DateTime.Parse(datetimestr)).ToList<Client>();
